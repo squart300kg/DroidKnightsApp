32cadc26d38f6da668d257ef36aa530d7459d3f3
sysong <sysong@pentasecurity.com>
2025-08-22 11:41:00 +0900

    (CHORE)[#554] 'core:ui' 'core.router.routerApi' 의존성 추가

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -12,4 +12,5 @@ android {
 dependencies {
     implementation(projects.core.designsystem)
     implementation(projects.core.domain.domainSessionApi)
+    implementation(projects.core.router.routerApi)
 }
